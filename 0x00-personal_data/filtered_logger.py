c6a81a3ed4ab351d02bdf170a01468ed49ca9aad
Emediong Francis <emediongfrancis@gmail.com>
2022-05-17 13:53:12 +0100

    Regexing log message

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module for handling Personal Data
+"""
+from typing import List
+import re
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """ Returns a log message obfuscated """
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
+    return message
