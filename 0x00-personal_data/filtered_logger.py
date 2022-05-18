c04549933b9fbd99b4914747912fa831a3cd8c51
Emediong Francis <emediongfrancis@gmail.com>
2022-05-18 07:53:45 +0100

    Connect to secure database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,6 +5,9 @@ Module for handling Personal Data
 from typing import List
 import re
 import logging
+from os import environ
+import mysql.connector
+
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
@@ -29,6 +32,20 @@ def get_logger() -> logging.Logger:
     logger.addHandler(stream_handler)
 
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ Returns a connector to a MySQL database """
+    username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = environ.get("PERSONAL_DATA_DB_NAME")
+
+    cnx = mysql.connector.connection.MySQLConnection(user=username,
+                                                     password=password,
+                                                     host=host,
+                                                     database=db_name)
+    return cnx
 
 
 class RedactingFormatter(logging.Formatter):
